0e6c6cad5e91a00032547a23126a2ec1b2bd3462
Mohamad Al Salty <mohamadhazems@gmail.com>
2024-06-21 11:22:18 -0300

    feat: adding some feautes

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ module "network" {
   subnet_services_name = var.subnet_services_name
 }
 
-# module "gke" {
-#   source               = "./gke"
-#   project_name         = var.project_name
-#   region               = var.region
-#   vpc_network          = module.network.vpc_network
-#   subnet_link          = module.network.vpc_subnet_nodes
-#   subnet_pods_name     = var.subnet_pods_name
-#   subnet_node_name     = var.subnet_node_name
-#   subnet_services_name = var.subnet_services_name
-#   k8s_cluster_name     = var.k8s_cluster_name
-# }
+module "gke" {
+  source               = "./gke"
+  project_name         = var.project_name
+  region               = var.region
+  vpc_network          = module.network.vpc_network
+  subnet_link          = module.network.vpc_subnet_nodes
+  subnet_pods_name     = var.subnet_pods_name
+  subnet_node_name     = var.subnet_node_name
+  subnet_services_name = var.subnet_services_name
+  k8s_cluster_name     = var.k8s_cluster_name
+}
