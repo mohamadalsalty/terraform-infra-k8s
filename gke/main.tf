6e4f3bd2636cbfded7969019a047e35891525e46
Mohamad Al Salty <mohamadhazems@gmail.com>
2024-07-02 16:33:07 -0300

    feat: changing a node param

--- gke/main.tf
+++ gke/main.tf
@@ -40,7 +40,7 @@ module "gke" {
 
   cluster_autoscaling = {
     enabled                     = true
-    min_cpu_cores               = 2
+    min_cpu_cores               = 5
     max_cpu_cores               = 30
     min_memory_gb               = 10
     max_memory_gb               = 70
